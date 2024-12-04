de6d135279a4b36d63c6018f18a5f4831bb376bd
Kaveesha Gimhana <36946467+KaveeshaG@users.noreply.github.com>
2024-12-05 01:53:06 +0530

    feat: added the gamini prompter UI

---
+++ src/app/components/gamini-prompter/gamini-prompter.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+interface Message {
+  content: string;
+  type: 'user' | 'assistant';
+  timestamp: Date;
+}
+
+@Component({
+  selector: 'app-gamini-prompter',
+  templateUrl: './gamini-prompter.component.html',
+  styleUrls: ['./gamini-prompter.component.scss']
+})
+export class GaminiPrompterComponent implements OnInit{
+
+  messages: Message[] = [];
+  currentTranscript = '';
+  isListening = false;
+  showRipple = false;
+  isProcessing = false;
+
+  ngOnInit(): void { }
+
+
+  public getTimeString(date: Date): string {
+    return date.toLocaleTimeString([], { 
+      hour: '2-digit', 
+      minute: '2-digit'
+    });
+  }
+
+}
